dd6f5eea50bae6bbf22fdc4f464eb1ef08df57cf
alok <alok.shrivastava1989@gmail.com>
2019-09-30 17:30:16 +0530

    added css learning

---
+++ src/test/java/com/vagrant/testCases/CssExample.java
@@ -0,0 +1,29 @@
+package com.vagrant.testCases;
+
+import java.util.concurrent.TimeUnit;
+
+import org.openqa.selenium.By;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
+
+import com.vagrant.baseClass.BaseClass;
+import com.vagrant.baseClass.VagrantInterface;
+import com.vagrant.pages.Vagrant_FlightBooking;
+
+public class CssExample extends BaseClass implements VagrantInterface {
+
+	@BeforeClass
+	public void init() {
+
+		driver.get(Config.getProperty("Testing_URL") + "flights");
+		driver.manage().timeouts().pageLoadTimeout(20, TimeUnit.SECONDS);
+	}
+
+	@Test(description = "finding xpath by css")
+	public void findXpathByCssAndPerformAction() {
+
+		driver.navigate().to("https://www.toolsqa.com/automation-practice-form/");
+		driver.findElement(By.cssSelector("input#lastname")).sendKeys("Shrivastava");
+		driver.findElement(By.cssSelector("button.btn btn-info")).click();
+	}
+}
